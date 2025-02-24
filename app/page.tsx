8aff1d333929c4f942a1be3dff8151061731cda4
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2025-02-24 06:48:01 +0200

    initial

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div
+    className="bg-violet-200  w-full border-2 border-violet-600 rounded-md my-4 p-2"
+    >
+      <h1 className="text-center font-mono font-extrabold text-[30px]">Hello Tailwind</h1>
+    </div>
+  );
+}
